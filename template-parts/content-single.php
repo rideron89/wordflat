fc673ee9be74304c01087e6b1fff697e6cc03120
Ron Rider <rideron89@gmail.com>
2015-09-25 01:50:36 +0000

    style body of single post page

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -9,27 +9,24 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-		<div class="entry-meta">
-			<?php wordflat_posted_on(); ?>
-		</div><!-- .entry-meta -->
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wordflat' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php wordflat_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
-
+<article id="post-<?php the_ID(); ?>" class="card main-content">
+	<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ) ?>
+    <sup class="entry-meta">by <?php the_author_posts_link() ?> on <?= get_the_date() ?> at <?php the_time() ?></sup>
+    <div class="entry-content"><?php the_content() ?></div>
+    <div class="entry-categories">
+        <?php if (get_the_tags()): ?>
+            <?php foreach (get_the_tags() as $tag): ?>
+                <a href="" class="badge"><?= $tag->name ?></a>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </div>
+    
+    <div class="row spacer"></div>
+    
+    <?php
+    	// If comments are open or we have at least one comment, load up the comment template.
+    	if ( comments_open() || get_comments_number() ) :
+    		comments_template();
+    	endif;
+    ?>
+</article>
