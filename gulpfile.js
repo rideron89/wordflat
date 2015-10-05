79441387d9178a2bcbbd27daca3cedb0a69e0847
Ron Rider <rideron89@gmail.com>
2015-10-05 10:45:32 -0400

    use the cssnano postcss plugin to minify CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
+    nano = require('gulp-cssnano'),
     path = require('path'),
+    postcss = require('gulp-postcss'),
     rename = require('gulp-rename'),
     rucksack = require('gulp-rucksack');
 
@@ -9,10 +11,20 @@ gulp.task('styles', function () {
         .pipe(less({
             paths: [ path.join(__dirname, 'less') ]
         }))
+        .pipe(postcss([
+        ]))
         .pipe(rucksack({
             autoprefixer: true,
             fallbacks: true
         }))
+        .pipe(nano())
+        .pipe(rename('styles.min.css'))
+        .pipe(gulp.dest('./assets'));
+});
+
+gulp.task('minify-styles', function() {
+    return gulp.src('./assets/styles.css')
+        .pipe(nano())
         .pipe(rename('styles.min.css'))
         .pipe(gulp.dest('./assets'));
 });
